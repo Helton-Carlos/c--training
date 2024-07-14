3dacd9dcceba46db19b32beea3ad584c046b32e8
Helton Brito <heltoncarlospa@hotmail.com>
2024-07-14 12:01:20 -0300

    feat: add validations in loops

--- Program.cs
+++ Program.cs
@@ -10,5 +10,43 @@ class Program
     ball.Unid = 4;
     ball.Price = 25M;
     Console.WriteLine(ball.Total);
+
+    Random dice = new Random();
+    int roll = dice.Next(1, 7);
+    Console.WriteLine($"Value Roll:{roll}");
+
+    int firstValue = 700;
+    int secondValue = 950;
+    int largerValue;
+    largerValue = Math.Max(firstValue, secondValue);
+    Console.WriteLine($"Max Value: {largerValue}");
+
+    string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+    foreach (string orderID in orderIDs)
+    {
+      if (orderID.StartsWith("C"))
+      {
+        Console.WriteLine(orderID);
+      }
+    }
+
+    //
+    string fruit = "cherry";
+
+    switch (fruit)
+    {
+      case "apple":
+        Console.WriteLine($"App will display information for apple.");
+        break;
+
+      case "banana":
+        Console.WriteLine($"App will display information for banana.");
+        break;
+
+      case "cherry":
+        Console.WriteLine($"App will display information for cherry.");
+        break;
+    }
   }
 }
